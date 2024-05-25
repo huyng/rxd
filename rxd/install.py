5420a24b0055c0085d6f719cf9f9e451b1337b28
Anon <anon@whatever>
2024-05-24 23:29:38 -0700

    updated install

--- rxd/install.py
+++ rxd/install.py
@@ -1,14 +1,20 @@
+import sys
 
-"""
+systemd_service = \
+f"""
+# save this into /etc/systemd/system/rxd.service
 [Unit]
 Description=rxd
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/python3 -m rxd.daemon
+ExecStart=/usr/bin/screen -dmS rxd {sys.executable} -m rxd.daemon
 TimeoutStopSec=5
 
 [Install]
 WantedBy=default.target
 """
+
+if __name__ == "__main__":
+    print(systemd_service)
